cce3761895fe3684b787cff280bbceb1ad23e66a
Shenghao Li <li.sh0629@outlook.com>
2024-11-02 10:46:11 +0800

    删除了默认css，添加了基础comtainer框架，安装了ol和elementplus包

---
+++ streetlight-vue/src/components/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="common-layout">
+    <el-container>
+      <el-header>Header</el-header>
+      <el-container>
+        <el-aside width="200px">Aside</el-aside>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.common-layout{
+  height: auto;
+  width: 100vw;
+}
+.el-header{
+  background-color: aqua;
+}
+.el-aside{
+  background-color: blueviolet;
+}
+.el-main{
+  background-color: brown;
+}
+</style>

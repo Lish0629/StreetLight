74d68e07699dac6041336d031be13ebf6ba8d23c
Shenghao Li <li.sh0629@outlook.com>
2024-11-24 09:53:10 +0800

    flask基础修改

--- streetlight-flask/app.py
+++ streetlight-flask/app.py
@@ -1,14 +1,30 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+from models import db, User
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
+db.init_app(app)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/users', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    return jsonify([user.to_dict() for user in users])
 
-@app.route('/test')
+@app.route('/test',methods=['GET'])
 def test():
-    return 'test'
+    return "test"
+
+@app.route('/users', methods=['POST'])
+def create_user():
+    data = request.json
+    new_user = User(name=data['name'], email=data['email'])
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify(new_user.to_dict()), 201
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()  # 创建数据库表
     app.run(debug=True)
---
+++ streetlight-flask/models.py
@@ -0,0 +1,17 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = 'lantern'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    status = db.Column(db.Boolean)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "status": self.status
+        }
